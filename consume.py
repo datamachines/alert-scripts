899295675a71aa8fac2c0d9e3416b497d7ab2b93
Courtney Kelly <ckelly19@nd.edu>
2017-05-23 15:07:58 -0700

    switched from webhook to RTM for consume script

--- consume.py
+++ consume.py
@@ -1,56 +1,68 @@
 #!/usr/bin/env python3
 
-import requests
-import json
 import yaml
 import time
 import argparse
+import logging
+from slackclient import SlackClient
 from os.path import expanduser
-
-# Constants 
-WEBHOOK = ''
-CHANNEL = ''
-USER = ''
-MESSAGE = ''
-
-# Main Execution
-
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Message a user on Slack')
-parser.add_argument('-w', type=str, help="webhook url of team to send results to", dest=WEBHOOK)
-parser.add_argument('-u', type=str, help="name of user to send results to", dest=USER)
-parser.add_argument('-c', type=str, help="name of channel to send results to", dest=CHANNEL)
-parser.add_argument('-m', type=str, help="message to send to user or channel", dest=MESSAGE)
-args = parser.parse_args()
 
 # Load message data
 data_file = expanduser("~") + '/.consume_data/data.yml'
 data = yaml.safe_load(open(data_file))
 
-# send to either a USER or a CHANNEL
-if data['USER'] != '':
-	payload = {
-		"text": data['MESSAGE'],
-		"channel": data['CHANNEL'],
-		"icon_emoji":":monkey_face:",
-		"username": "George"
-	}
-else: 
-	payload = {
-		"text": data['MESSAGE'],
-		"user": data['USER'],
-		"icon_emoji":":monkey_face:",
-		"username": "George"
-	}
+# Constants 
+TOKEN = data['TOKEN']
+CHANNEL = data['CHANNEL']
+MESSAGE = data['MESSAGE']
+OK = True
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-# consume channel with posts
-print("consume.py: consuming...")
+# Main Execution
 
-r = requests.post(data['WEBHOOK'], data = json.dumps(payload))
-COUNT = 1
-while r.text == 'ok':
-	r = requests.post(data['WEBHOOK'], data = json.dumps(payload))
-	COUNT += 1
-	time.sleep(1) # API allows 1 message per second 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='consume a channel on Slack')
+parser.add_argument('-t', type=str, help="API token of team to send results to", dest=TOKEN)
+parser.add_argument('-c', type=str, help="name or ID of channel to consume", dest=CHANNEL)
+parser.add_argument('-m', type=str, help="message to send to consume channel", dest=MESSAGE)
+args = parser.parse_args()
 
-print("consume.py: sent {} messages before reaching rate limit".format(COUNT))
+# create client
+sc = SlackClient(TOKEN)
+
+# start connection
+logging.info("starting connection...")
+if sc.rtm_connect():  # connect to a Slack RTM websocket
+	logging.info("connection successful...")
+	time.sleep(1) # wait for response
+
+	# read connection response
+	logging.debug("[RTM Websocket Response] {}".format(sc.rtm_read())) # read initial response
+	logging.debug("[RTM Websocket Response] {}".format(sc.rtm_read())) # read initial response
+	
+	# get channel
+	logging.info("locating channel \'{}\'...".format(CHANNEL))
+	response = sc.server.channels.find(CHANNEL)
+	CHANNEL_ID = response.id
+
+	# consume until error
+	logging.info("consuming...")
+	while OK:
+		sc.rtm_send_message(CHANNEL_ID, MESSAGE) # send message
+		time.sleep(1)	# wait one second
+		response = sc.rtm_read()
+		if response:
+			if 'ok' in response[0]:	# skip other types of responses (i.e. 'reconnect_url')
+				OK = response[0]['ok']
+			elif 'type' in response[0]:
+				if response[0]['type'] == 'error':
+					OK = False
+		logging.debug("[RTM Websocket Response] {}".format(response)) # read response
+
+	# print error 
+	logging.error("[RTM Websocket Response] Send Message Error: {}".format(response[0]['error']['msg']))
+else:
+    logging.error('[RTM Websocket Connection] FAILED: possible invalid token')
+
+# exit on failure
+logging.info("exiting...")
